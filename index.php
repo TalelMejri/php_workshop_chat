fcd88a8b2e62fb6d189e9e70c34d05eba11b0454
TalelMejri <talelmejri8@gmail.com>
2025-04-24 18:19:55 +0100

    affiche messages

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
-
-   $pdo=new PDO("mysql:host=localhost;dbname=chatphp","root","");
     
+    $pdo=new PDO("mysql:host=localhost;dbname=chatphp","root","");
+
+    $messages=$pdo->query("select * from messages");
+    $data=$messages->fetchAll();
+
+
+ include("layout.phtml")   ;
+
 
 
 ?>
