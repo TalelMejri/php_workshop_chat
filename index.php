9f15bf6262cfedb22fac2ebd3171378bf752589b
TalelMejri <talelmejri8@gmail.com>
2025-04-24 18:57:50 +0100

    add message

--- index.php
+++ index.php
@@ -5,7 +5,18 @@
     $messages=$pdo->query("select * from messages");
     $data=$messages->fetchAll();
 
+
+    if(isset($_POST['submit'])){
+        $message=$_POST['message'];
+        $date = new DateTime();
+        $formattedDate = $date->format("Y-m-d H:i:s");
+        $pdo->query("INSERT INTO `messages`( `message`, `date`) VALUES ('$message','$formattedDate')");
+    }
+    $messages=$pdo->query("select * from messages");
+    $data=$messages->fetchAll();
+
     include("layout.phtml");
+
 
 
 
