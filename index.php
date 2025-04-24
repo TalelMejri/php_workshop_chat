01aebb80b57cfa2fd2fa63e1e730d53dc4b3ace6
TalelMejri <talelmejri8@gmail.com>
2025-04-24 19:12:01 +0100

    delete

--- index.php
+++ index.php
@@ -12,7 +12,12 @@
         $formattedDate = $date->format("Y-m-d H:i:s");
         $pdo->query("INSERT INTO `messages`( `message`, `date`) VALUES ('$message','$formattedDate')");
     }
-    
+
+    if(isset($_GET['id'])){
+        $id=$_GET['id'];
+        $pdo->query("DELETE FROM messages where Id=$id");
+    }
+
     $messages=$pdo->query("select * from messages");
     $data=$messages->fetchAll();
 
